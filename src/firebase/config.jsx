9b35fbc002ff991e4834854b42aeb7304eb4e391
oxlzlo <yeojin58@naver.com>
2024-05-28 16:23:14 +0900

    format: 전체 파일 prettier 포맷

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,7 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, addDoc, doc, updateDoc, deleteDoc, getDocs } from 'firebase/firestore';
+import {
+	getFirestore,
+	collection,
+	addDoc,
+	doc,
+	updateDoc,
+	deleteDoc,
+	getDocs
+} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 

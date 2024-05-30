2613b57593a17a53fd2310e0c1cbccdf8dcc1ade
oxlzlo <yeojin58@naver.com>
2024-05-30 17:43:02 +0900

    feat: 월별 총수익 확인 박스 레이아웃 구현

---
+++ src/components/EarningCheck.jsx
@@ -0,0 +1,35 @@
+import EarningDetails from './EarningDetails';
+import ArrowLeft from '../assets/arrow_left.png';
+import ArrowRight from '../assets/arrow_right.png';
+
+function EarningCheck() {
+	return (
+		<>
+			<div className="earningCheck">
+				<div className="earningCheck__inner">
+					<div className="earningCheck__inner_title">5월 수익</div>
+					<div className="earningCheck__inner_sub">총 1,100,000원</div>
+					<div className="earningCheck__inner_contentsBox">
+						<div className="earningCheck__inner_contentsBox_contents">
+							<EarningDetails />
+							<EarningDetails />
+							<EarningDetails />
+							<EarningDetails />
+							<EarningDetails />
+						</div>
+						<div className="earningCheck__inner_contentsBox_arrow">
+							<img src={ArrowLeft} alt="ArrowLeft" />
+							<img src={ArrowRight} alt="ArrowRight" />
+						</div>
+					</div>
+				</div>
+				<div className="earningCheck__inner">
+					<div className="earningCheck__inner_title">5월 수익</div>
+					<div className="earningCheck__inner_sub">종류별 내역</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default EarningCheck;

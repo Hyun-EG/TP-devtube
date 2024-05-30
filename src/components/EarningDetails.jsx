2059f6de0b587dacd3fc3ccc0a8b8f2a4b803ed9
oxlzlo <yeojin58@naver.com>
2024-05-30 18:01:20 +0900

    feat: 총수익 내역 임시 설정(디자인)

---
+++ src/components/EarningDetails.jsx
@@ -0,0 +1,14 @@
+function EarningDetails() {
+	return (
+		<div className="earning_detail">
+			<div className="earning_detail_grid">
+				<div className="earning_detail_grid_content">2024.05.30</div>
+				<div className="earning_detail_grid_content">광고</div>
+				<div className="earning_detail_grid_content_check">명세서 확인</div>
+				<div className="earning_detail_grid_content_amount">3,000,000원</div>
+			</div>
+		</div>
+	);
+}
+
+export default EarningDetails;

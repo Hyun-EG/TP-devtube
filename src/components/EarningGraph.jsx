ec557e08994dc0548c265d9420c7ca4aa61a6911
oxlzlo <yeojin58@naver.com>
2024-05-31 18:01:35 +0900

    design: 그래프 색상 변경

--- src/components/EarningGraph.jsx
+++ src/components/EarningGraph.jsx
@@ -1,5 +1,6 @@
-import RectangleBlue from '../assets/rectangle_blue.png';
-import RectangleGray from '../assets/rectangle_gray.png';
+import RectangleRed from '../assets/rectangle_red.png';
+import RectangleLightblue from '../assets/rectangle_lightblue.png';
+
 import Graph from './Graph';
 
 function EarningGraph() {
@@ -8,9 +9,9 @@ function EarningGraph() {
 			<div className="graph">
 				<div className="graph__inner">
 					<div className="graph__inner_box">
-						<img src={RectangleBlue} alt="RectangleBlue" />
+						<img src={RectangleRed} alt="RectangleRed" />
 						<span className="graph__inner_box_name">수익</span>
-						<img src={RectangleGray} alt="RectangleGray" />
+						<img src={RectangleLightblue} alt="RectangleLightblue" />
 						<span className="graph__inner_box_name">조회수</span>
 					</div>
 				</div>

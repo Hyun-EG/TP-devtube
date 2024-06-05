ae7c616b88768f1efd4a6aa0fe995d25198b6a73
oxlzlo <yeojin58@naver.com>
2024-06-05 15:13:19 +0900

    fix: 모달창 열렸을 때 백그라운드에 border선 뚜렷한거 해결

--- src/components/EarningModal.jsx
+++ src/components/EarningModal.jsx
@@ -5,35 +5,43 @@ function EarningModal({ isOpen }) {
 	const dispatch = useDispatch();
 	const selectedEarning = useSelector(state => state.earnings.selectedEarning);
 
-	if (!isOpen || !selectedEarning) return null;
+	if (!selectedEarning) {
+		return null;
+	}
 
 	return (
 		<div className="earning_modal">
-			<div className="earning_modal_title">수익명세서</div>
-			<div className="earning_modal_inner">
-				<div className="earning_modal_inner_subtitle">
-					<div className="earning_modal_inner_subtitle_content">지급 날짜</div>
-					<div className="earning_modal_inner_subtitle_detail">
-						{selectedEarning.date}
+			<div className="earning_modal_wrapper">
+				<div className="earning_modal_wrapper_title">수익명세서</div>
+				<div className="earning_modal_wrapper_inner">
+					<div className="earning_modal_wrapper_inner_subtitle">
+						<div className="earning_modal_wrapper_inner_subtitle_content">
+							지급 날짜
+						</div>
+						<div className="earning_modal_wrapper_inner_subtitle_detail">
+							{selectedEarning.date}
+						</div>
+					</div>
+					<div className="earning_modal_wrapper_inner_subtitle">
+						<div className="earning_modal_wrapper_inner_subtitle_content">
+							수익 종류
+						</div>
+						<div className="earning_modal_wrapper_inner_subtitle_detail">
+							{selectedEarning.type}
+						</div>
 					</div>
 				</div>
-				<div className="earning_modal_inner_subtitle">
-					<div className="earning_modal_inner_subtitle_content">수익 종류</div>
-					<div className="earning_modal_inner_subtitle_detail">
-						{selectedEarning.type}
+				<div className="earning_modal_wrapper_total">
+					<div className="earning_modal_wrapper_total_content">지급 총액</div>
+					<div className="earning_modal_wrapper_total_content">
+						{selectedEarning.amount.toLocaleString()}원
 					</div>
 				</div>
-			</div>
-			<div className="earning_modal_total">
-				<div className="earning_modal_total_content">지급 총액</div>
-				<div className="earning_modal_total_content">
-					{selectedEarning.amount.toLocaleString()}원
+				<div
+					className="earning_modal_wrapper_button"
+					onClick={() => dispatch(closeModal())}>
+					<div className="earning_modal_wrapper_button_content">확인</div>
 				</div>
-			</div>
-			<div
-				className="earning_modal_button"
-				onClick={() => dispatch(closeModal())}>
-				<div className="earning_modal_button_content">확인</div>
 			</div>
 		</div>
 	);
--- src/pages/EarningsPage.jsx
+++ src/pages/EarningsPage.jsx
@@ -2,9 +2,21 @@ import { useSelector } from 'react-redux';
 import EarningCheck from '../components/EarningCheck';
 import EarningGraph from '../components/EarningGraph';
 import EarningModal from '../components/EarningModal';
+import { useEffect } from 'react';
 
 function EarningsPage() {
 	const isModalOpen = useSelector(state => state.earnings.isModalOpen);
+
+	useEffect(() => {
+		const modalEl = document.querySelector('.earning_modal');
+		if (modalEl) {
+			if (isModalOpen) {
+				document.querySelector('.earning_modal').classList.add('modal-open');
+			} else {
+				document.querySelector('.earning_modal').classList.remove('modal-open');
+			}
+		}
+	}, [isModalOpen]);
 
 	return (
 		<>

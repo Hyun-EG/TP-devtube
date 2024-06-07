f8f4b02341e7e1688b5ec2645ba16041fb9636ed
oxlzlo <yeojin58@naver.com>
2024-06-07 20:50:47 +0900

    fix: 정정신청 데이터 파이어베이스에 연결 안 되는 에러 해결

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,59 +1,79 @@
-import { collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import {
+	collection,
+	addDoc,
+	deleteDoc,
+	doc,
+	serverTimestamp
+} from 'firebase/firestore';
 import { useReducer } from 'react';
 import { db } from '../firebase/config';
 
 const initState = {
-  document: null,
-  isPending: false,
-  error: null,
-  success: false
-}
+	document: null,
+	isPending: false,
+	error: null,
+	success: false
+};
 
 const storeReducer = (state, action) => {
-  switch (action.type){
-    case 'isPending':
-      return { isPending: true, document: null, success: false, error: null }
-    case 'addDoc':
-      return { isPending: false, document: action.payload, success: true, error: null }
-    case 'error':
-      return { isPending: false, document: null, success: false, error: action.payload }
-    case 'deleteDoc':
-      return { isPending: false, document: action.payload, success: true, error: null }
-    default :
-      return state
-  }
-}
+	switch (action.type) {
+		case 'isPending':
+			return { isPending: true, document: null, success: false, error: null };
+		case 'addDoc':
+			return {
+				isPending: false,
+				document: action.payload,
+				success: true,
+				error: null
+			};
+		case 'error':
+			return {
+				isPending: false,
+				document: null,
+				success: false,
+				error: action.payload
+			};
+		case 'deleteDoc':
+			return {
+				isPending: false,
+				document: action.payload,
+				success: true,
+				error: null
+			};
+		default:
+			return state;
+	}
+};
 
-export const useFirestore = (transaction) => {
+export const useFirestore = transaction => {
+	const [response, dispatch] = useReducer(storeReducer, initState);
 
-  const [response, dispatch] = useReducer(storeReducer, initState)
+	// colRef: 컬렉션의 참조 요구
+	const colRef = collection(db, transaction);
 
-  // colRef: 컬렉션의 참조 요구
-  const colRef = collection(db, transaction);
+	// 컬렉션에 문서 추가
+	const addDocument = async doc => {
+		dispatch({ type: 'isPending' });
 
-  // 컬렉션에 문서 추가
-  const addDocument = async (doc) => {
-    dispatch({ type: "isPending" });
+		try {
+			const createdTime = serverTimestamp();
+			const docRef = await addDoc(colRef, { ...doc, createdTime });
+			dispatch({ type: 'addDoc', payload: docRef });
+		} catch (error) {
+			dispatch({ type: 'error', payload: error.message });
+		}
+	};
 
-    try {
-      const createdTime = timestamp.fromDate(new Date());
-      const docRef = await addDoc(colRef, { ...doc, createdTime });
-      dispatch({ type: "addDoc", payload: docRef });
-    } catch (error) {
-      dispatch({ type: "error", payload: error.message });
-    }
-  }
+	// 컬렉션에 문서 제거
+	const deleteDocument = async id => {
+		dispatch({ type: 'isPending' });
 
-  // 컬렉션에 문서 제거
-  const deleteDocument = async (id) => {
-    dispatch({ type: "isPending" });
-
-    try {
-      const docRef = await deleteDoc(doc(colRef, id));
-      dispatch({ type: "deleteDoc", payload: docRef });
-    } catch (error) {
-      dispatch({ type: "error", payload: error.message });
-    }
-  }
-  return { addDocument, deleteDocument, response }
-}
+		try {
+			await deleteDoc(doc(colRef, id));
+			dispatch({ type: 'deleteDoc', payload: id });
+		} catch (error) {
+			dispatch({ type: 'error', payload: error.message });
+		}
+	};
+	return { addDocument, deleteDocument, response };
+};

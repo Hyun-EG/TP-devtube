07d7300d78caa10806da689e2aca5d197998d63f
oxlzlo <yeojin58@naver.com>
2024-06-07 19:14:19 +0900

    design: 로딩 애니메이션 디자인 구현

--- src/components/RequestList.jsx
+++ src/components/RequestList.jsx
@@ -1,33 +1,32 @@
 import noData from '../assets/no_data.png';
+import Loading from './Loading';
 
 function RequestList() {
-  return (
-    <>
-      <div className="requests">
-        <div className="header">
-          <h1 className="board_title">신청 내역</h1>
-        </div>
+	return (
+		<>
+			<div className="requests">
+				<div className="header">
+					<h1 className="board_title">신청 내역</h1>
+				</div>
 
-        <div className="contents">
-          <input className="btn_delete"
-            type="button" 
-            value="선택 삭제"
-          />
-        </div>
-        <div className="requests_list">
-          <div className="contents">
-            <div className="item">
-              <img src={ noData } alt="no data" />
-            </div>
-            <div className="item">
-              <span className="text">신청 내역이 없습니다.</span>
-              <p>정정 신청 해보세요.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+				<div className="contents">
+					<input className="btn_delete" type="button" value="선택 삭제" />
+				</div>
+				<div className="requests_list">
+					<div className="contents">
+						<div className="item">
+							<img src={noData} alt="no data" />
+						</div>
+						<div className="item">
+							<span className="text">신청 내역이 없습니다.</span>
+							<p>정정 신청 해보세요.</p>
+						</div>
+					</div>
+					<Loading />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default RequestList;
